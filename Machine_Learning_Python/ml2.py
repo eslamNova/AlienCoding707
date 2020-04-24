b617d5d8ff7c4b5c6831051f0e8906e72e7e607a
alien <alien@owl.com>
2020-04-24 17:57:47 +0200

    update ml2.py

--- Machine_Learning_Python/ml2.py
+++ Machine_Learning_Python/ml2.py
@@ -1,5 +1,11 @@
 import pandas as pd
-import quandl
+import quandl, math
+import numpy as np 
+from sklearn import preprocessing, svm
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression
+
+
 quandl.ApiConfig.api_key= 'B-thwqnXzarybtigLyyH'
 
 df = quandl.get('WIKI/GOOGL')
@@ -10,4 +16,31 @@ df['HL_PCT'] = (df['Adj. High'] - df['Adj. Close']) / df['Adj. Close'] * 100
 df['PCT_change'] = (df['Adj. Close'] - df['Adj. Open']) / df['Adj. Open'] * 100
 
 df = df [['Adj. Close', 'HL_PCT', 'PCT_change', 'Adj. Volume']]
-print(df.head)
+
+forcast_col = 'Adj. Close'
+df.fillna(-99999, inplace=True)
+
+forcast_out = int(math.ceil(0.01*len(df)))
+print(forcast_out)
+
+df['label'] = df[forcast_col].shift(-forcast_out)
+
+df.dropna(inplace=True)
+
+X = np.array(df.drop(['label'], 1))
+y = np.array(df['label'])
+
+X = preprocessing.scale(X)
+
+y = np.array(df['label'])
+
+
+#train
+
+X_train, X_test, y_train, y_test = train_test_split(X,y,test_size=0.2)
+
+clf = LinearRegression()
+clf.fit(X_train, y_train)
+accuracy =  clf.score(X_test, y_test)
+
+print(accuracy)
