2b7b5337d8e512c6b66a479ae4001d3c49d24ec0
alien <eslam77s_nova@outlook.com>
2020-05-02 11:07:24 +0200

    update code

--- Algorithms_Implementation/sorting_algorithms_intersect.py
+++ Algorithms_Implementation/sorting_algorithms_intersect.py
@@ -6,14 +6,26 @@ B = [3,3,7,15,31]
 # print(set(A).intersection(B))
 
 
-
+# O(n + m)
 def intersect_sorted_array(A, B):
     S = []
-    for i in A:
-        for j in B:
-            if i == j:
-                S.append(i)
-    return set(S)
+    i = 0
+    j = 0
+    while i < len(A) and j < len(B):
+        if A[i] == B[j]:
+            if i == 0 or A[i] != A[i-1]:
+                S.append(A[i])
+            j += 1
+            i += 1
+        elif A[i] < B[j]:
+            i += 1
+        else:
+            j += 1
+    # for i in A:
+    #     for j in B:
+    #         if i == j:
+    #             S.append(i)
+    return S
 
 
 print(intersect_sorted_array(A, B))
