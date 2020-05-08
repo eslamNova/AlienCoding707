9a7bd5438d71ba7256ea11733c06941ecc00212a
alien <eslam77s_nova@outlook.com>
2020-05-08 17:31:59 +0200

    end of ch1 in ml book

--- Books/RealWorldMachineLearning/initial_structure.py
+++ Books/RealWorldMachineLearning/initial_structure.py
@@ -2,3 +2,13 @@ data = load_data("data/people.csv")
 model = build_model(data, target="Marital status")
 new_data = load_data("data/new_people.csv")
 predictions = model.predict(new_data)
+
+
+###
+
+data = load_data(...)
+training_data, testing_data = split_data(data)
+model = build_model(training_data, target="Marital status")
+true_values = testing_data.extract_column("Marital status")
+predictions = model.predict(testing_data)
+accuracy = compare_predictions(predictions, true_values)
