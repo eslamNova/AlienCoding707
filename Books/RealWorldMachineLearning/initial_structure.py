cbd0b907d4af892b2809fd49034cf19c825cefbf
alien <eslam77s_nova@outlook.com>
2020-05-10 20:19:48 +0200

    in ch2

--- Books/RealWorldMachineLearning/initial_structure.py
+++ Books/RealWorldMachineLearning/initial_structure.py
@@ -1,3 +1,5 @@
+### chapter 1
+
 data = load_data("data/people.csv")
 model = build_model(data, target="Marital status")
 new_data = load_data("data/new_people.csv")
@@ -12,3 +14,14 @@ model = build_model(training_data, target="Marital status")
 true_values = testing_data.extract_column("Marital status")
 predictions = model.predict(testing_data)
 accuracy = compare_predictions(predictions, true_values)
+
+
+### chapter 2
+
+def cat_to_num(data):
+categories = unique(data)
+features = []
+for cat in categories:
+binary = (data == cat)
+features.append(binary.astype("int"))
+return features
